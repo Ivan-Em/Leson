cae055781cd5c95abd9398b9a253009a45cc92d2
Ivan E <asvool@list.ru>
2023-03-07 11:37:37 +0300

    Lesons_1

---
+++ Lesons/src/Main.java
@@ -0,0 +1,13 @@
+
+
+
+class MyProgram{
+    public static void main(String[] args){
+        //для Git
+        int[] a = new int[]{1, 2, 3};
+        int[] c = a;
+        c[0] = 5;
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+    }
+}
